ccd24fdbd6e16e7141430e8ed2b7a5ab2908c4f2
David Tolnay <dtolnay@gmail.com>
2017-10-28 18:17:14 -0400

    Move compile tests into a test-only crate

--- compiletest/src/lib.rs
+++ compiletest/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg(test)]
+
 extern crate compiletest_rs as compiletest;
 
 use std::env;
@@ -6,11 +8,11 @@ fn run_mode(mode: &'static str) {
     let mut config = compiletest::Config::default();
 
     config.mode = mode.parse().expect("invalid mode");
-    config.target_rustcflags = Some("-L target/debug/deps".to_owned());
+    config.target_rustcflags = Some("-L ../target/debug/deps".to_owned());
     if let Ok(name) = env::var("TESTNAME") {
         config.filter = Some(name);
     }
-    config.src_base = format!("tests/{}", mode).into();
+    config.src_base = mode.into();
 
     compiletest::run_tests(&config);
 }

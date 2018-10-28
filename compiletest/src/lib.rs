d933041b24617f706e4b263d43ec7dfb0e0864b7
David Tolnay <dtolnay@gmail.com>
2018-10-28 09:46:06 -0700

    Simplify compile-fail config setup

--- compiletest/src/lib.rs
+++ compiletest/src/lib.rs
@@ -2,22 +2,16 @@
 
 extern crate compiletest_rs as compiletest;
 
-use std::env;
-
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
-
-    config.mode = mode.parse().expect("invalid mode");
-    config.target_rustcflags = Some("-L ../target/debug/deps".to_owned());
-    if let Ok(name) = env::var("TESTNAME") {
-        config.filter = Some(name);
-    }
-    config.src_base = mode.into();
-
-    compiletest::run_tests(&config);
-}
-
 #[test]
 fn compile_fail() {
-    run_mode("compile-fail");
+    let mut config = compiletest::Config {
+        mode: compiletest::common::Mode::CompileFail,
+        src_base: std::path::PathBuf::from("compile-fail"),
+        ..Default::default()
+    };
+
+    config.link_deps();
+    config.clean_rmeta();
+
+    compiletest::run_tests(&config);
 }

83c3547ca391f59f78765ede7ceb151af8a55c11
David Tolnay <dtolnay@gmail.com>
2019-02-17 11:48:08 -0800

    Simplify running update-references.sh

--- compiletest/main.rs
+++ compiletest/main.rs
@@ -14,6 +14,7 @@ fn compile_fail() {
              --extern ref_cast \
              ",
         )),
+        build_base: std::path::PathBuf::from("../target/ui"),
         ..Default::default()
     };
 

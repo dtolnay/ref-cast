17910142be046ee82b4185a548c5a37074ad2ebd
David Tolnay <dtolnay@gmail.com>
2022-10-09 22:29:03 -0700

    Release 1.0.10

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/ref-cast/1.0.9")]
+#![doc(html_root_url = "https://docs.rs/ref-cast/1.0.10")]
 #![no_std]
 #![allow(
     clippy::manual_assert,

06c520cd555920951cc1939fcb0c5d855b61134c
David Tolnay <dtolnay@gmail.com>
2024-05-06 21:07:37 -0700

    Release 1.0.23

--- src/lib.rs
+++ src/lib.rs
@@ -135,7 +135,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/ref-cast/1.0.22")]
+#![doc(html_root_url = "https://docs.rs/ref-cast/1.0.23")]
 #![no_std]
 #![allow(
     clippy::extra_unused_type_parameters,
